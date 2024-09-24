b90210c40cf7f6e2567512dcc50c159468e960f1
willdoesprojects <sunsoffire@gmail.com>
2024-09-24 17:00:34 -0400

    used diff hog

--- backend/main.py
+++ backend/main.py
@@ -11,51 +11,15 @@ from tensorflow import keras
 from tensorflow.keras.models import load_model
 import sys
 import pickle
-from test_model import train_and_save_models
-
 
 app = Flask(__name__)
 CORS(app)
 
-print("Creating Model")
-
-from image_processing import load_image, compute_hog
-from model import train_kmeans, train_svm, load_model
-from utils import load_dataset, time_execution
-import numpy as np
-
-# Load the data
-dataset = load_dataset('./data', img_size=(256, 256))
-
-# Extract images and labels
-dogs, cats = [], []
-for images_batch, labels_batch in dataset:
-    for image, label in zip(images_batch, labels_batch):
-        if label == 1:
-            dogs.append(image.numpy())
-        else:
-            cats.append(image.numpy())
-
-# Compute HOG features
-hog_features = [compute_hog(image)[1] for image in dogs + cats]
-
-# Train and Save Models
-kmeans, visual_words = train_kmeans(hog_features)
-bow_vectors = np.zeros((len(hog_features), 1000))
-for i, word in enumerate(visual_words):
-    bow_vectors[i, word] += 1
-labels = [1] * len(dogs) + [0] * len(cats)
-classifier = train_svm(bow_vectors, labels)
-
-
-# train_and_save_models()
-
-# # Load your pre-trained models
-# with open('models/kmeans.pkl', 'rb') as f:
-#     kmeans = pickle.load(f)
-# with open('models/classifier.pkl', 'rb') as f:
-#     classifier = pickle.load(f)
-
+# Load your pre-trained models
+with open('models/kmeans.pkl', 'rb') as f:
+    kmeans = pickle.load(f)
+with open('models/classifier.pkl', 'rb') as f:
+    classifier = pickle.load(f)
 model = load_model('models/dog_cat_classifier.h5')
 
 # Import your compute_hog function
--- backend/test_model.py
+++ backend/test_model.py
@@ -1,45 +1,29 @@
-import numpy as np
-import time
 from image_processing import load_image, compute_hog
 from model import train_kmeans, train_svm, load_model
 from utils import load_dataset, time_execution
+import numpy as np
+import time
 
-def train_and_save_models(data_path='./data', img_size=(256, 256), n_clusters=1000):
-    """
-    Function to load data, compute HOG features, and train KMeans and SVM models.
-    """
-    # Load the data
-    dataset = load_dataset(data_path, img_size=img_size)
+# Load the data
+dataset = load_dataset('./data', img_size=(256, 256))
 
-    # Extract images and labels
-    dogs, cats = [], []
-    for images_batch, labels_batch in dataset:
-        for image, label in zip(images_batch, labels_batch):
-            if label == 1:
-                dogs.append(image.numpy())
-            else:
-                cats.append(image.numpy())
+# Extract images and labels
+dogs, cats = [], []
+for images_batch, labels_batch in dataset:
+    for image, label in zip(images_batch, labels_batch):
+        if label == 1:
+            dogs.append(image.numpy())
+        else:
+            cats.append(image.numpy())
+            
+# Compute HOG features
+hog_features = [compute_hog(image) for image in dogs + cats]
 
-    # Compute HOG features for the images
-    hog_features = [compute_hog(image)[1] for image in dogs + cats]
-
-    # Train KMeans and get visual words
-    kmeans, visual_words = train_kmeans(hog_features, n_clusters=n_clusters)
-
-    # Build Bag of Words (BoW) vectors
-    bow_vectors = np.zeros((len(hog_features), n_clusters))
-    for i, word in enumerate(visual_words):
-        bow_vectors[i, word] += 1
-
-    # Generate labels (1 for dogs, 0 for cats)
-    labels = [1] * len(dogs) + [0] * len(cats)
-
-    # Train the SVM classifier
-    classifier = train_svm(bow_vectors, labels)
-
-    print("Model training and saving completed.")
-    return 
-
-if __name__ == '__main__':
-    # Call the function in main.py
-    train_and_save_models()
+# Train and Save Models
+kmeans, visual_words = train_kmeans(hog_features)
+bow_vectors = np.zeros((len(hog_features), 1000))
+for i, word in enumerate(visual_words):
+    bow_vectors[i, word] += 1
+labels = [1] * len(dogs) + [0] * len(cats)
+classifier = train_svm(bow_vectors, labels)
+print("done")
